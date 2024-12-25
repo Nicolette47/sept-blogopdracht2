6750ed60f173356e61120c96ad367c7537d9df71
nvanzeijl <nvanzeijl@gmail.com>
2024-12-25 18:15:20 +0100

    opdracht 2 en 3 klaar

--- src/pages/overviewPosts/OverviewPosts.jsx
+++ src/pages/overviewPosts/OverviewPosts.jsx
@@ -2,43 +2,54 @@ import React from 'react';
 import './OverviewPosts.css';
 import axios from 'axios';
 import {useEffect} from "react";
-import {Link} from ' react-router-dom';
+import {Link} from 'react-router-dom';
 
 
 const OverviewPosts = () => {
 
     const [overviewPosts, setOverviewPosts] = React.useState([]);
+    const [error, toggleError] = React.useState('');
 
     useEffect(() => {
         fetchPosts()
     }, []);
 
     async function fetchPosts() {
+        toggleError(false);
         try {
             const result = await axios.get('http://localhost:3000/posts');
             setOverviewPosts(result.data);
 
         } catch (error) {
-            console.log(error);
+            toggleError(true);
         }
     }
 
     return (
-        <div>
+
+        <>
             <h1>Bekijk alle {overviewPosts.length} posts op het platform</h1>
 
+            {error &&
+                <> <p> Er is iets mis gegaan met het ophalen van de gegevens. Druk op deze knop om opnieuw te
+                    proberen</p>
+                    <button type="button" className="error-button" onClick={fetchPosts}>Klik hier</button>
+                </>}
+
+            {overviewPosts.length > 0 &&
             <ul>
                 {overviewPosts.map((post) => {
                     return (
                         <li key={post.id} className="post-card">
-                            <p><strong> <Link to>{post.title} </Link></strong>({post.author}) </p>
+                            <p><Link to={`/postdetail/${post.id}`}
+                                     className="blog-title">{post.title}</Link> ({post.author})</p>
                             <p><em>{post.comments} reacties - {post.shares} keer gedeeld</em></p>
                         </li>
                     );
                 })}
             </ul>
-
-        </div>
+            }
+        </>
     );
 };
 
--- src/pages/postdetailpage/Postdetail.jsx
+++ src/pages/postdetailpage/Postdetail.jsx
@@ -1,18 +1,61 @@
 import React from 'react';
 import './Postdetail.css';
 import {useParams} from 'react-router-dom';
-
+import axios from 'axios';
+import {useEffect} from "react";
+import {Link} from 'react-router-dom';
+import dateCambio from '../../helpers/dateCambio.js';
 
 
 const Postdetail = () => {
+    const [blogDetail, setBlogDetail] = React.useState({});
+    const [error, toggleError] = React.useState('');
 
     const {postId} = useParams();
 
+    useEffect(() => {
+        fetchBlog()
+    }, []);
+
+    async function fetchBlog() {
+        toggleError(false);
+
+        try {
+            const result = await axios.get(`http://localhost:3000/posts/${postId}`);
+            setBlogDetail(result.data);
+        } catch (error) {
+            console.error(error);
+            toggleError(true);
+        }
+    }
+
+
     return (
-        <div>
-            <h2>Dit is de blogpostdetail page nummer {postId}</h2>
-        </div>
-    );
+        <>
+            {error &&
+                <>
+                    <p> Er is iets mis gegaan met het ophalen van de gegevens. Druk op deze knop om opnieuw te
+                    proberen</p>
+                    <button type="button" className="error-button" onClick={fetchBlog}>Klik hier</button>
+                </>
+            }
+
+            {Object.keys(blogDetail).length > 0 &&
+                <div className="blogdetail-container">
+
+                    <>
+                        <h1>{blogDetail.title}</h1>
+                        <h2>{blogDetail.subtitle}</h2>
+                        <p>Geschreven door {blogDetail.author} op {dateCambio(blogDetail.created)}</p>
+                        <p>{blogDetail.content}</p>
+                        <p>{blogDetail.comments} reacties - {blogDetail.shares} keer gedeeld</p>
+                        <p><Link to="/overviewposts">&lt; Terug naar de overzichtspagina</Link></p>
+                    </>
+                </div>
+            }
+        </>
+    )
+        ;
 };
 
 export default Postdetail;
